687dfabb9b6d7de8a2fe2a8cbf79b29c1195a093
Sanjsigan <sanjsi0507@gmail.com>
2021-11-28 10:22:30 +0530

    create firebase config functions

---
+++ src/components/SignIn.js
@@ -0,0 +1,11 @@
+import React from 'react' 
+
+function SignIn(){
+    return (
+        <div>
+            <button>Signin with Google</button>
+        </div>
+    )
+} 
+
+export default SignIn
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyAcP3U_mshMzUYnZDVMq-r_3tR_CZANCbo",
+    authDomain: "chatfire-af25a.firebaseapp.com",
+    projectId: "chatfire-af25a",
+    storageBucket: "chatfire-af25a.appspot.com",
+    messagingSenderId: "420852166858",
+    appId: "1:420852166858:web:e264167791a9f1e0650433",
+    measurementId: "G-VC5M3YBH7B"
+
+})
