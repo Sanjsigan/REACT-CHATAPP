12732278fd3ee8125cbc766fd14d43e928041be8
Sanjsigan <sanjsi0507@gmail.com>
2021-11-30 11:48:27 +0530

    signout, show message done

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import {db} from '../firebase'
 import SignOut from './SignOut'
 
 function Chat(){
+    const [message, setMessage]=useState([]);
 
+    useEffect(() => {
+        db.collection('messages').orderBy('createdAt').limit(50).onSnapshot(snapshot => {
+            setMessage(snapshot.docs.map(doc => doc.data())) 
+        })
+    }, [])
     return (
         <div>
          <SignOut/>
+         {message.map(({id, text, photoURL})=>(
+                <div key={id}>
+                    <img src={photoURL} alt=""/>
+                    <p>{text}</p>
+                </div>
+         ))}
         </div>
     )
 } 
--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/auth';
 
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyAcP3U_mshMzUYnZDVMq-r_3tR_CZANCbo",
-    authDomain: "chatfire-af25a.firebaseapp.com",
-    projectId: "chatfire-af25a",
-    storageBucket: "chatfire-af25a.appspot.com",
-    messagingSenderId: "420852166858",
-    appId: "1:420852166858:web:e264167791a9f1e0650433",
-    measurementId: "G-VC5M3YBH7B"
+    apiKey: "AIzaSyB1KIyhy6BhBCTXlOKZvs-iMn--Ur3bY1k",
+    authDomain: "newchat-20b5e.firebaseapp.com",
+    projectId: "newchat-20b5e",
+    storageBucket: "newchat-20b5e.appspot.com",
+    messagingSenderId: "76609399306",
+    appId: "1:76609399306:web:1b590eea97645c8d9bcdf3",
+    measurementId: "G-8M4SYLJLZW"  
 
 })  
 
